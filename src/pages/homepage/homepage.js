c494dda6a979821ccf13a682a0f65ba826c92c3d
rpai <rahul.pai@aptushealth.com>
2019-12-12 14:26:40 +0530

    division of homepage into smaller components and styling of the directory menu-item componenet of the homepage

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/homepage/homepage.scss";
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+);
+
+export default HomePage;
